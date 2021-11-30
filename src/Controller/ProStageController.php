7c7e48e8d53bd93a98cc7e0dff88a35d8ae2c0d5
Kevin GAREL <kgarel@iutbayonne.univ-pau.fr>
2021-11-30 16:08:06 +0100

    Création des premières routes

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,11 +9,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage_accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+    /**
+     * @Route("/entreprises", name="pro_stage_listeEntreprises")
+     */
+    public function listeEntreprises(): Response
+    {
+        return $this->render('pro_stage/listeEntreprises.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+    /**
+     * @Route("/formations", name="pro_stage_listeFormations")
+     */
+    public function listeFormations(): Response
+    {
+        return $this->render('pro_stage/listeFormations.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+    /**
+     * @Route("/stages", name="pro_stage_stages")
+     */
+    public function stages(): Response
+    {
+        return $this->render('pro_stage/stages.html.twig', [
             'controller_name' => 'ProStageController',
         ]);
     }
