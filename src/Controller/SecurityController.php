73b2a70fd6e93f540a14ab745ee1036cc7d7d961
Kevin GAREL <kgarel@iutbayonne.univ-pau.fr>
2022-03-23 18:54:52 +0100

    Formulaire d'authentification fonctionnel, affichage en fonction de l'état de la connexion fonctionnel, déconnexion fonctionnelle, décomposition fonctionnelle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="pro_stage_login")
+     * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="pro_stage_logout")
+     * @Route("/logout", name="app_logout")
      */
     public function logout(): void
     {
